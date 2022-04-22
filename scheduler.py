2800db24227ea26a52e567f9fcaaabbf03b756bf
jasonlightmatter <jason@lightmatter.com>
2022-04-21 20:51:43 -0400

    adding generate model logic to scheduler

--- scheduler.py
+++ scheduler.py
@@ -10,13 +10,22 @@ sched = BlockingScheduler()
 sched.configure(timezone=utc)
 
 # 2 seconds before every 5 minute interval
-@sched.scheduled_job('cron', minute='4-59/5', second='58')
-def timed_job():
+@sched.scheduled_job('cron', minute='4-59/5', second='56')
+def predict_5min_interval():
     now = datetime.datetime.utcnow()
-    print ("Current date and time : ")
-    print (now.strftime("%Y-%m-%d %H:%M:%S"))
+    print (f"timed_job_5min_interval(): {now.strftime("%Y-%m-%d %H:%M:%S")}")
 
     # trade.py --pair XRP/USDT --interval 5m --candles 50 --loadCloudModel  
     p = subprocess.run(['python', 'trade.py', '--pair', 'XRP/USDT', '--interval', '5m', '--candles', '50', '--loadCloudModel'])
+
+
+# Generate a model 30 minutes after every hour
+@sched.scheduled_job('cron', minute='30')
+def generate_model_30min_after_the_hour():
+    now = datetime.datetime.utcnow()
+    print (f"generate_model_30min_after_the_hour(): {now.strftime("%Y-%m-%d %H:%M:%S")}")
+
+    # python algo_trader.py --pair XRP/USDT --interval 5m --candles 50 --epochs 1 --learning_rate 0.03  --cloudStorage
+    p = subprocess.run(['python', 'algo_trader.py', '--pair', 'XRP/USDT', '--interval', '5m', '--candles', '50', '--epochs', '18', '--learning_rate 0.03', '--cloudStorage'])
 
 sched.start()
