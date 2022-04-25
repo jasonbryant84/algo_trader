d107cfaa6199368f5f4ff9ac50fc15b03ed30234
jasonlightmatter <jason@lightmatter.com>
2022-04-25 11:52:37 -0600

    debugged the scheduler's model logic

--- scheduler.py
+++ scheduler.py
@@ -11,13 +11,13 @@ sched.configure(timezone=utc)
 
 pair = 'XRP/USDT'
 interval_str = '5m'
-interval = int(interval_str[:-1])
+interval_num = interval_str[:-1]
 n_candles = '50'
-n_epochs = 18
-learning_rate = 0.03
+n_epochs = '18'
+learning_rate = '0.03'
 
 # 2 seconds before every 5 minute interval
-@sched.scheduled_job('cron', minute=f"4-59/{interval}", second='56')
+@sched.scheduled_job('cron', minute=f"4-59/{interval_num}", second='56')
 def predict_on_interval():
     now = datetime.datetime.utcnow()
     print (f"--- predict_on_interval ({interval_str} interval): {now.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -37,7 +37,7 @@ def generate_model_30min_after_the_hour():
 
 
 # Generate a model 30 minutes after every hour
-@sched.scheduled_job('cron', minute=f"*/{interval}", second='30')
+@sched.scheduled_job('cron', minute=f"*/{interval_num}", second='30')
 def update_predictions_metrics():
     now = datetime.datetime.utcnow()
     print (f"--- update_predictions_metrics ({interval_str} interval): {now.strftime('%Y-%m-%d %H:%M:%S')}")
