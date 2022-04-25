8dde55822375acf6a4ddb3709ed5ba200578925d
jasonlightmatter <jason@lightmatter.com>
2022-04-25 10:09:51 -0600

    parameterize scheduler a bit

--- scheduler.py
+++ scheduler.py
@@ -9,34 +9,41 @@ from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 sched = BlockingScheduler()
 sched.configure(timezone=utc)
 
+pair = 'XRP/USDT'
+interval_str = '5m'
+interval = int(interval_str[:-1])
+n_candles = '50'
+n_epochs = 18
+learning_rate = 0.03
+
 # 2 seconds before every 5 minute interval
-@sched.scheduled_job('cron', minute='4-59/5', second='56')
-def predict_5min_interval():
+@sched.scheduled_job('cron', minute=f"4-59/{interval}", second='56')
+def predict_on_interval():
     now = datetime.datetime.utcnow()
-    print (f"timed_job_5min_interval(): {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    print (f"--- predict_on_interval ({interval_str} interval): {now.strftime('%Y-%m-%d %H:%M:%S')}")
 
     # trade.py --pair XRP/USDT --interval 5m --candles 50 --loadCloudModel  
-    p = subprocess.run(['python', 'trade.py', '--pair', 'XRP/USDT', '--interval', '5m', '--candles', '50', '--loadCloudModel'])
+    p = subprocess.run(['python', 'trade.py', '--pair', pair, '--interval', interval_str, '--candles', n_candles, '--loadCloudModel'])
 
 
 # Generate a model 30 minutes after every hour
 @sched.scheduled_job('cron', minute='*/30')
 def generate_model_30min_after_the_hour():
     now = datetime.datetime.utcnow()
-    print (f"generate_model_30min_after_the_hour(): {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    print (f"generate_model_30min_after_the_hour ({interval_str} interva): {now.strftime('%Y-%m-%d %H:%M:%S')}")
 
     # python algo_trader.py --pair XRP/USDT --interval 5m --candles 50 --epochs 1 --learning_rate 0.03  --cloudStorage
-    p = subprocess.run(['python', 'algo_trader.py', '--pair', 'XRP/USDT', '--interval', '5m', '--candles', '50', '--epochs', '18', '--learning_rate', '0.03', '--cloudStorage'])
+    p = subprocess.run(['python', 'algo_trader.py', '--pair', pair, '--interval', interval_str, '--candles', n_candles, '--epochs', n_epochs, '--learning_rate', learning_rate, '--cloudStorage'])
 
 
 # Generate a model 30 minutes after every hour
-@sched.scheduled_job('cron', minute='*/15')
+@sched.scheduled_job('cron', minute=f"*/{interval}", second='30')
 def update_predictions_metrics():
     now = datetime.datetime.utcnow()
-    print (f"update_predictions_metrics(): {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    print (f"--- update_predictions_metrics ({interval_str} interval): {now.strftime('%Y-%m-%d %H:%M:%S')}")
 
     # python check_trades.py --pair XRP/USDT --interval 5m --candles 50 --cloudStorage
-    p = subprocess.run(['python', 'check_trades.py', '--pair', 'XRP/USDT', '--interval', '5m', '--candles', '50', '--cloudStorage'])
+    p = subprocess.run(['python', 'check_trades.py', '--pair', pair, '--interval', interval_str, '--candles', n_candles, '--cloudStorage'])
 
 
 sched.start()
